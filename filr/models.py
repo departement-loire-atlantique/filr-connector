84b95be352e1f73ea5be03e05191f6cdf32b0709
Guillaume GAUTIER <guillaume.gautier@loire-atlantique.fr>
2022-04-19 11:03:42 +0200

    Endpoint uploadsimple (pour tester hors WS Publik) et upload basique et fonctionnel.

--- filr/models.py
+++ filr/models.py
@@ -1,7 +1,10 @@
 import base64
+import binascii
 import datetime
-from typing import OrderedDict
 import requests
+
+from collections import OrderedDict
+
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils import six
@@ -15,6 +18,52 @@ from passerelle.utils.conversion import to_ascii
 from passerelle.utils.jsonresponse import APIError
 from passerelle.utils.validation import is_number
 
+ADD_DOCUMENT_SCHEMA = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'title': 'File to upload',
+    'description': '',
+    'type': 'object',
+    'required': ['folder_id', 'login', 'password', 'document'],
+    'properties': OrderedDict(
+        {
+            'folder_id': {
+                'description': 'Filr folder',
+                'type': 'string',
+            },
+            'login': {
+                'description': 'Filr login',
+                'type': 'string',
+            },
+            'password': {
+                'description': 'Filr password',
+                'type': 'string',
+            },
+            'filename': {
+                'description': 'Filr filename',
+                'type': 'string',
+            },
+            'document': {
+                'type': 'object',
+                'description': 'File object (file0)',
+                'required': ['filename', 'content_type', 'content'],
+                'properties': {
+                    'filename': {
+                        'type': 'string',
+                        'description': 'Filr filename',
+                    },
+                    'content_type': {
+                        'type': 'string',
+                        'description': 'MIME content-type',
+                    },
+                    'content': {
+                        'type': 'string',
+                        'description': 'Content, base64 encoded',
+                    },
+                },
+            },
+        }
+    ),
+}
 
 class Filr(BaseResource):
     category = "Divers"
@@ -24,89 +73,26 @@ class Filr(BaseResource):
 
     api_description = "API pour Filr."
 
-    # Création d'un objet contenant les praramètres pour ajouter un champs d'upload de document
-    DOCUMENT_SCHEMA = {
-        "$schema": "http://json-schema.org/draft-04/schema#",
-        "title": "Ajouter un document",
-        "description": "",
-        "type": "object",
-        "required": ["demand_id", "document", "title"],
-        "properties": OrderedDict({
-            "demand_id": {
-                "description": "Document title (docTitle)",
-                "type": "string",
-            },
-            "title": {
-                "description": "Document Title (docTitle)",
-                "type": "string"
-            },
-            "filename": {
-                "description": "Document filename (docFile)",
-                "type": "string"
-            },
-            "document": {
-                "type": "object",
-                "description": "File object (file0)",
-                "required": ["filename", "content_type", "content"],
-                "properties": {
-                    "filename": {
-                        "type": "string"
-                    },
-                    "content_type": {
-                        "type": "string",
-                        "description": "MIME content-type"
-                    },
-                    "content": {
-                        "type": "string",
-                        "description": "Content, base64 encoded"
-                    }
-                }
-            }
-        })
-    }
-
     @endpoint()
     def info(self, request):
-        return {"hello": "Filr!"}
+        return {"hello": "Filr! ça marche"}
+
+    @endpoint(perm="can_access")
+    def upload(self, request):
+        return {"hello": "Filr upload!"}
 
     @endpoint(
-        name = "add-document",
-        description = "",
-        perm = "can_access",
-        methods = ["post"],
-        post={
-            "request_body": {
-                "schema": {
-                    "application/json": DOCUMENT_SCHEMA
-                }
-            }
-        }
-    )
-    def add_document(self, request, post_data):
-        endpoint = "api/export/document/test/" + post_data["demand_id"]
-        filename = post_data.get("filename") or post_data["document"]["filename"]
-        params = {
-            "docTitle": post_data["title"],
-            "docFile": filename
-        }
-        content = base64.b64decode(post_data["document"]["content"])
-        content_type = post_data["document"]["content_type"]
-        files = {
-            "file0": (filename, content, content_type)
-        }
-        added = self.call(endpoint, method="post", params=params, files=files)
-        return {"data": added}
-        
-
-
-    @endpoint(
-        name="upload",
+        name="uploadsimple",
         methods=["post"],
         description=_("Upload a file"),
         parameters={
             "WFID": {
                 "description": _("ID du worklow emetteur"),
                 "example_value": "DIA-ENS",
+            },
+            "folder_id": {
+                "description": _("ID du dossier Filr"),
+                "example_value": "855055",
             },
             "emails": {
                 "description": _("emails des destinataires"),
@@ -121,27 +107,38 @@ class Filr(BaseResource):
                 "example_value": "password",
             },
         },
-        post={
-            "request_body": {
-                "schema": {
-                    "application/json": DOCUMENT_SCHEMA
-                }
-            }
-        }
     )
-    def upload(self, request, WFID, emails, login, password):
-        code, label, gid_per = self.call_select_appairage(login, password, email)
-        if code in ["2", "3", "5"]:
-            link, created = Link.objects.get_or_create(
-                resource=self, name_id=NameID, id_per=id_per
-            )
-            return {"link_id": link.pk, "new": created, "code": code, "label": label}
-        elif code == "6":
-            raise APIError("unknown-login", data={"code": code, "label": label})
-        elif code in ["4", "1"]:
-            raise APIError("invalid-password", data={"code": code, "label": label})
 
+    def uploadsimple(self, request, folder_id, login, password, **kwargs):
 
-    def send_file(self, request, user, password, folder_id, post_data):
-        # TODO : Setup post request to https://transfert.loire-atlantique.fr/rest/folders/{folder_id}?file_name=aaa.txt&overwrite_existing=true
-        pass
+        file_name = 'test_upload_from_passerelle_01.txt'
+        file_path = '/home/guillaume/dev/filr-connector/filr/' + file_name
+
+        url = 'https://transfert.loire-atlantique.fr/rest/folders/' + folder_id + '/library_files?file_name=' + file_name
+
+        with open(file_path, 'rb') as payload:
+            headers = {'content-type': 'application/octet-stream'}
+            r = requests.post(url, auth=(login, password), data=payload, headers=headers)
+
+        return {}
+
+    @endpoint(
+        name="upload",
+        perm='can_access',
+        methods=["post"],
+        description=_("Upload a file"),
+        post={'request_body': {'schema': {'application/json': ADD_DOCUMENT_SCHEMA}}},
+        display_category=_('Demand'),
+        display_order=1,
+    )
+
+    def upload(self, request, post_data):
+
+        login = post_data.get('login')
+        password = post_data.get('password')
+        filename = post_data.get('filename') or post_data['document']['filename']
+        endpoint = 'https://transfert.loire-atlantique.fr/rest/folders/' + post_data['folder_id'] + '/library_files?file_name=' + filename
+        content = base64.b64decode(post_data['document']['content'])
+        headers = {'content-type': 'application/octet-stream'}
+        r = requests.post(endpoint, auth=(login, password), data=content, headers=headers)
+        return {'data': {"commentaire": r.text}}
